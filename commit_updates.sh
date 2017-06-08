350b8825ae56514c5bf662dfcf6da881d9660c47
Travis CI <travis@travis-ci.org>
2017-06-07 23:31:06 -0400

    modifying script

--- commit_updates.sh
+++ commit_updates.sh
@@ -1,19 +1,21 @@
+#https://gist.github.com/willprice/e07efd73fb7f13f917ea
+
 setup_git() {
   git config --global user.email "travis@travis-ci.org"
   git config --global user.name "Travis CI"
 }
 
 commit_website_files() {
-  #git checkout -b gh-pages
+  git clone --depth=50 --branch=master https://github.com/pjreiniger/maven_repo.git pjreiniger/maven_repo
   git add .
   git commit --message "Travis build: $TRAVIS_BUILD_NUMBER"
 }
 
 upload_files() {
-  git remote add origin-pages https://${GH_TOKEN}@github.com/MVSE-outreach/resources.git > /dev/null 2>&1
-  git push --quiet --set-upstream origin-pages gh-pages 
+  git remote add origin-pages https://${GH_TOKEN}@github.com/pjreiniger/maven_repo.git > /dev/null 2>&1
+  git push https://${GH_TOKEN}@github.com/pjreiniger/maven_repo.git
 }
 
 setup_git
 commit_website_files
-#upload_files
+upload_files
