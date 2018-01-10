15bac9a2b102eb2694e51d0eee19a4aaf3bbd68a
PJ Reiniger <pj.reiniger@gmail.com>
2018-01-10 00:02:52 -0500

    Adding windows

--- windows_helper.bat
+++ windows_helper.bat
@@ -1,32 +1,35 @@
 
+SET RELEASE_VERSION=0.7.0
+
+
 :: Add necessary files
-git add com/snobot/simulator/snobot_sim_java/0.5/snobot_sim_java-0.5-uber_native-windows.jar*
-git add com/snobot/simulator/wpilib/0.5/wpilib-0.5-wpi_native-windows.zip*
-git add com/snobot/simulator/navx_sim/navx_simulator/0.5/navx_simulator-0.5.dll*
-git add com/snobot/simulator/ctre_override/0.5/ctre_override-0.5-uber_native-windows.jar*
+git add com/snobot/simulator/snobot_sim_java/%RELEASE_VERSION%/snobot_sim_java-%RELEASE_VERSION%-uber_native-windows.jar*
+git add com/snobot/simulator/wpilib/%RELEASE_VERSION%/wpilib-%RELEASE_VERSION%-wpi_native-windows.zip*
+git add com/snobot/simulator/navx_sim/navx_simulator/%RELEASE_VERSION%/navx_simulator-%RELEASE_VERSION%.dll*
+git add com/snobot/simulator/ctre_override/%RELEASE_VERSION%/ctre_override-%RELEASE_VERSION%-uber_native-windows.jar*
 
 :: Revert files that don't need to be override
-git checkout com/snobot/simulator/snobot_sim_java/0.5/snobot_sim_java-0.5-javadoc.jar*
-git checkout com/snobot/simulator/snobot_sim_java/0.5/snobot_sim_java-0.5-sources.jar*
-git checkout com/snobot/simulator/snobot_sim_java/0.5/snobot_sim_java-0.5-sources.zip*
-git checkout com/snobot/simulator/snobot_sim_java/0.5/snobot_sim_java-0.5.jar*
-git checkout com/snobot/simulator/snobot_sim_java/0.5/snobot_sim_java-0.5.pom*
+git checkout com/snobot/simulator/snobot_sim_java/%RELEASE_VERSION%/snobot_sim_java-%RELEASE_VERSION%-javadoc.jar*
+git checkout com/snobot/simulator/snobot_sim_java/%RELEASE_VERSION%/snobot_sim_java-%RELEASE_VERSION%-sources.jar*
+git checkout com/snobot/simulator/snobot_sim_java/%RELEASE_VERSION%/snobot_sim_java-%RELEASE_VERSION%-sources.zip*
+git checkout com/snobot/simulator/snobot_sim_java/%RELEASE_VERSION%/snobot_sim_java-%RELEASE_VERSION%.jar*
+git checkout com/snobot/simulator/snobot_sim_java/%RELEASE_VERSION%/snobot_sim_java-%RELEASE_VERSION%.pom*
 git checkout com/snobot/simulator/snobot_sim_java/maven-metadata.xml*
 
-git checkout com/snobot/simulator/navx_sim/navx_simulator/0.5/navx_simulator-0.5-headers.zip*
-git checkout com/snobot/simulator/navx_sim/navx_simulator/0.5/navx_simulator-0.5-sources.zip*
-git checkout com/snobot/simulator/navx_sim/navx_simulator/0.5/navx_simulator-0.5.pom*
+git checkout com/snobot/simulator/navx_sim/navx_simulator/%RELEASE_VERSION%/navx_simulator-%RELEASE_VERSION%-headers.zip*
+git checkout com/snobot/simulator/navx_sim/navx_simulator/%RELEASE_VERSION%/navx_simulator-%RELEASE_VERSION%-sources.zip*
+git checkout com/snobot/simulator/navx_sim/navx_simulator/%RELEASE_VERSION%/navx_simulator-%RELEASE_VERSION%.pom*
 git checkout com/snobot/simulator/navx_sim/navx_simulator/maven-metadata.xml*
 
-git checkout com/snobot/simulator/ctre_override/0.5/ctre_override-0.5.jar*
-git checkout com/snobot/simulator/ctre_override/0.5/ctre_override-0.5.pom*
+git checkout com/snobot/simulator/ctre_override/%RELEASE_VERSION%/ctre_override-%RELEASE_VERSION%.jar*
+git checkout com/snobot/simulator/ctre_override/%RELEASE_VERSION%/ctre_override-%RELEASE_VERSION%.pom*
 git checkout com/snobot/simulator/ctre_override/maven-metadata.xml*
 
 git checkout com/snobot/simulator/snobot_sim_gui
 git checkout com/snobot/simulator/snobot_sim_utilites
 
-git checkout com/snobot/simulator/wpilib/0.5/wpilib-0.5-wpi_headers.zip*
-git checkout com/snobot/simulator/wpilib/0.5/wpilib-0.5.pom*
+git checkout com/snobot/simulator/wpilib/%RELEASE_VERSION%/wpilib-%RELEASE_VERSION%-wpi_headers.zip*
+git checkout com/snobot/simulator/wpilib/%RELEASE_VERSION%/wpilib-%RELEASE_VERSION%.pom*
 git checkout com/snobot/simulator/wpilib/maven-metadata.xml*
 
 :: Remove unecessary files
