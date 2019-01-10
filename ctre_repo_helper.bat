6fa0e969ee54487c28524aff69f323a6336a99c0
PJ Reiniger <pj.reiniger@gmail.com>
2019-01-09 22:23:54 -0500

    Adding windows libraries

--- ctre_repo_helper.bat
+++ ctre_repo_helper.bat
@@ -1,14 +1,18 @@
 
-SET RELEASE_VERSION=V1_5.2.1.1
+SET RELEASE_VERSION=5.12.0_V0_RC
 
 
 :: Add necessary files
-git add com/snobot/simulator/ctre_sim_override/%RELEASE_VERSION%/ctre_sim_override-%RELEASE_VERSION%-windows-x86.jar*
-git add com/snobot/simulator/ctre_sim_override/%RELEASE_VERSION%/ctre_sim_override-%RELEASE_VERSION%-windows-x86-64.jar*
+git add com/snobot/simulator/ctre_sim_override/%RELEASE_VERSION%/ctre_sim_override-%RELEASE_VERSION%-windowsx86.jar*
+git add com/snobot/simulator/ctre_sim_override/%RELEASE_VERSION%/ctre_sim_override-%RELEASE_VERSION%-windowsx86*.zip
+git add com/snobot/simulator/ctre_sim_override/%RELEASE_VERSION%/ctre_sim_override-%RELEASE_VERSION%-windowsx86-64.jar*
+git add com/snobot/simulator/ctre_sim_override/%RELEASE_VERSION%/ctre_sim_override-%RELEASE_VERSION%-windowsx86-64*.zip
 
 :: Revert files that don't need to be override
 git checkout com/snobot/simulator/ctre_sim_override/%RELEASE_VERSION%/ctre_sim_override-%RELEASE_VERSION%.jar*
 git checkout com/snobot/simulator/ctre_sim_override/%RELEASE_VERSION%/ctre_sim_override-%RELEASE_VERSION%-javadoc.jar*
 git checkout com/snobot/simulator/ctre_sim_override/%RELEASE_VERSION%/ctre_sim_override-%RELEASE_VERSION%-sources.jar*
+git checkout com/snobot/simulator/ctre_sim_override/%RELEASE_VERSION%/ctre_sim_override-%RELEASE_VERSION%-sources.zip*
+git checkout com/snobot/simulator/ctre_sim_override/%RELEASE_VERSION%/ctre_sim_override-%RELEASE_VERSION%-headers.zip*
 git checkout com/snobot/simulator/ctre_sim_override/%RELEASE_VERSION%/ctre_sim_override-%RELEASE_VERSION%.pom*
-git checkout com/snobot/simulator/snobot_sim_java/maven-metadata.xml*
+git checkout com/snobot/simulator/ctre_sim_override/maven-metadata.xml*
