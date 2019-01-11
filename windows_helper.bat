67d6c1b5a2b5e86bb67ef5fe3aade585cc4e18c7
PJ Reiniger <pj.reiniger@gmail.com>
2019-01-10 20:56:04 -0500

    Adding windows release

--- windows_helper.bat
+++ windows_helper.bat
@@ -1,21 +1,24 @@
 @echo off
 
-SET RELEASE_VERSION=2019-0.0.0
+SET RELEASE_VERSION=2019-0.0.1-RC
 
 
 :: Add necessary files
 git add com/snobot/simulator/navx_simulator/%RELEASE_VERSION%/navx_simulator-%RELEASE_VERSION%-windowsx86.jar*
-git add com/snobot/simulator/navx_simulator/%RELEASE_VERSION%/navx_simulator-%RELEASE_VERSION%-windowsx86.zip
+git add com/snobot/simulator/navx_simulator/%RELEASE_VERSION%/navx_simulator-%RELEASE_VERSION%-windowsx86*.zip
 git add com/snobot/simulator/navx_simulator/%RELEASE_VERSION%/navx_simulator-%RELEASE_VERSION%-windowsx86-64.jar*
-git add com/snobot/simulator/navx_simulator/%RELEASE_VERSION%/navx_simulator-%RELEASE_VERSION%-windowsx86-64.zip
+git add com/snobot/simulator/navx_simulator/%RELEASE_VERSION%/navx_simulator-%RELEASE_VERSION%-windowsx86-64*.zip
 git add com/snobot/simulator/adx_family/%RELEASE_VERSION%/adx_family-%RELEASE_VERSION%-windowsx86.jar*
-git add com/snobot/simulator/adx_family/%RELEASE_VERSION%/adx_family-%RELEASE_VERSION%-windowsx86.zip*
-git add com/snobot/simulator/adx_family/%RELEASE_VERSION%/adx_family-%RELEASE_VERSION%-windowsx86-64.zip*
+git add com/snobot/simulator/adx_family/%RELEASE_VERSION%/adx_family-%RELEASE_VERSION%-windowsx86*.zip*
+git add com/snobot/simulator/adx_family/%RELEASE_VERSION%/adx_family-%RELEASE_VERSION%-windowsx86-64*.zip*
 git add com/snobot/simulator/adx_family/%RELEASE_VERSION%/adx_family-%RELEASE_VERSION%-windowsx86-64.jar*
 git add com/snobot/simulator/snobot_sim_jni/%RELEASE_VERSION%/snobot_sim_jni-%RELEASE_VERSION%-windowsx86.jar*
-git add com/snobot/simulator/snobot_sim_jni/%RELEASE_VERSION%/snobot_sim_jni-%RELEASE_VERSION%-windowsx86.zip
+git add com/snobot/simulator/snobot_sim_jni/%RELEASE_VERSION%/snobot_sim_jni-%RELEASE_VERSION%-windowsx86*.zip
 git add com/snobot/simulator/snobot_sim_jni/%RELEASE_VERSION%/snobot_sim_jni-%RELEASE_VERSION%-windowsx86-64.jar*
-git add com/snobot/simulator/snobot_sim_jni/%RELEASE_VERSION%/snobot_sim_jni-%RELEASE_VERSION%-windowsx86-64.zip
+git add com/snobot/simulator/snobot_sim_jni/%RELEASE_VERSION%/snobot_sim_jni-%RELEASE_VERSION%-windowsx86-64*.zip
+
+git add com/snobot/simulator/snobot_sim/%RELEASE_VERSION%/snobot_sim-%RELEASE_VERSION%-windowsx86*.zip*
+git add com/snobot/simulator/snobot_sim/%RELEASE_VERSION%/snobot_sim-%RELEASE_VERSION%-windowsx86-64*.zip
 
 :: Revert files that don't need to be override
 git checkout com/snobot/simulator/wpilib
@@ -25,6 +28,7 @@ git checkout com/snobot/simulator/snobot_sim_java/%RELEASE_VERSION%/snobot_sim_j
 git checkout com/snobot/simulator/snobot_sim_java/%RELEASE_VERSION%/snobot_sim_java-%RELEASE_VERSION%.jar*
 git checkout com/snobot/simulator/snobot_sim_java/%RELEASE_VERSION%/snobot_sim_java-%RELEASE_VERSION%.pom*
 git checkout com/snobot/simulator/snobot_sim_java/maven-metadata.xml*
+git checkout com/snobot/simulator/snobot_sim_java/maven-metadata-local.xml*
 
 git checkout com/snobot/simulator/navx_simulator/%RELEASE_VERSION%/navx_simulator-%RELEASE_VERSION%.jar*
 git checkout com/snobot/simulator/navx_simulator/%RELEASE_VERSION%/navx_simulator-%RELEASE_VERSION%-sources.jar*
@@ -33,6 +37,7 @@ git checkout com/snobot/simulator/navx_simulator/%RELEASE_VERSION%/navx_simulato
 git checkout com/snobot/simulator/navx_simulator/%RELEASE_VERSION%/navx_simulator-%RELEASE_VERSION%-javadoc.jar*
 git checkout com/snobot/simulator/navx_simulator/%RELEASE_VERSION%/navx_simulator-%RELEASE_VERSION%.pom*
 git checkout com/snobot/simulator/navx_simulator/maven-metadata.xml*
+git checkout com/snobot/simulator/navx_simulator/maven-metadata-local.xml*
 
 git checkout com/snobot/simulator/adx_family/%RELEASE_VERSION%/adx_family-%RELEASE_VERSION%-javadoc.jar*
 git checkout com/snobot/simulator/adx_family/%RELEASE_VERSION%/adx_family-%RELEASE_VERSION%-sources.jar*
@@ -41,10 +46,13 @@ git checkout com/snobot/simulator/adx_family/%RELEASE_VERSION%/adx_family-%RELEA
 git checkout com/snobot/simulator/adx_family/%RELEASE_VERSION%/adx_family-%RELEASE_VERSION%.jar*
 git checkout com/snobot/simulator/adx_family/%RELEASE_VERSION%/adx_family-%RELEASE_VERSION%.pom*
 git checkout com/snobot/simulator/adx_family/maven-metadata.xml*
+git checkout com/snobot/simulator/adx_family/maven-metadata-local.xml*
 
 git checkout com/snobot/simulator/snobot_sim/%RELEASE_VERSION%/snobot_sim-%RELEASE_VERSION%-sources.zip*
+git checkout com/snobot/simulator/snobot_sim/%RELEASE_VERSION%/snobot_sim-%RELEASE_VERSION%-headers.zip*
 git checkout com/snobot/simulator/snobot_sim/%RELEASE_VERSION%/snobot_sim-%RELEASE_VERSION%.pom*
 git checkout com/snobot/simulator/snobot_sim/maven-metadata.xml*
+git checkout com/snobot/simulator/snobot_sim/maven-metadata-local.xml*
 
 git checkout com/snobot/simulator/snobot_sim_jni/%RELEASE_VERSION%/snobot_sim_jni-%RELEASE_VERSION%-javadoc.jar*
 git checkout com/snobot/simulator/snobot_sim_jni/%RELEASE_VERSION%/snobot_sim_jni-%RELEASE_VERSION%-sources.jar*
@@ -53,6 +61,7 @@ git checkout com/snobot/simulator/snobot_sim_jni/%RELEASE_VERSION%/snobot_sim_jn
 git checkout com/snobot/simulator/snobot_sim_jni/%RELEASE_VERSION%/snobot_sim_jni-%RELEASE_VERSION%.jar*
 git checkout com/snobot/simulator/snobot_sim_jni/%RELEASE_VERSION%/snobot_sim_jni-%RELEASE_VERSION%.pom*
 git checkout com/snobot/simulator/snobot_sim_jni/maven-metadata.xml*
+git checkout com/snobot/simulator/snobot_sim_jni/maven-metadata-local.xml*
 
 git checkout com/snobot/simulator/snobot_sim_gui
 git checkout com/snobot/simulator/snobot_sim_utilities
